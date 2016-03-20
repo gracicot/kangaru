fa31f0c8c629b3e586d9dd784d723b5e44c90141
Guillaume Racicot <gufideg@gmail.com>
2016-03-20 17:06:29 +0000

    added lazy services

--- include/kangaru/operator.hpp
+++ include/kangaru/operator.hpp
@@ -30,4 +30,130 @@ private:
 	kgr::Container _container;
 };
 
+namespace detail {
+
+template<typename T>
+struct LazyHelper {
+protected:
+	using type = T;
+	using ref = T&;
+	using ptr = T*;
+	
+	T&& assign(T&& service) {
+		return std::move(service);
+	}
+	
+	T& value(T& service) {
+		return service;
+	}
+};
+
+template<typename T>
+struct LazyHelper<T&> {
+protected:
+	using type = T*;
+	using ref = T&;
+	using ptr = T*;
+	
+	T* assign(T& service) {
+		return &service;
+	}
+	
+	T& value(T* service) {
+		return *service;
+	}
+};
+
+template<typename CRTP, typename T>
+struct LazyBase : LazyHelper<ServiceType<T>> {
+private:
+	using typename detail::LazyHelper<ServiceType<T>>::type;
+	using typename detail::LazyHelper<ServiceType<T>>::ref;
+	using typename detail::LazyHelper<ServiceType<T>>::ptr;
+	
+public:
+	LazyBase() = default;
+	LazyBase(LazyBase&& other) {
+		if (other._initialized) {
+			emplace(std::move(other.get()));
+		}
+	}
+	
+	LazyBase& operator=(LazyBase&& other) {
+		if (other._initialized) {
+			emplace(std::move(other.get()));
+		}
+		return *this;
+	}
+	
+	LazyBase(const LazyBase& other) {
+		if (other._initialized) {
+			emplace(other.get());
+		}
+	}
+	
+	LazyBase& operator=(const LazyBase& other) {
+		if (other._initialized) {
+			emplace(other.get());
+		}
+		return *this;
+	}
+	
+	~LazyBase() {
+		if (_initialized) {
+			get().~ServiceType<T>();
+		}
+	}
+	
+	ref operator*() {
+		return get();
+	}
+	
+	ptr operator->() {
+		return &get();
+	}
+	
+	ref get() {
+		if (!_initialized) {
+			emplace(this->assign(static_cast<CRTP*>(this)->_container.template service<T>()));
+		}
+		
+		return this->value(*reinterpret_cast<type*>(&_service));
+	}
+	
+private:
+	template<typename... Args>
+	void emplace(Args&&... args) {
+		if (_initialized) {
+			get().~ServiceType<T>();
+		}
+		
+		_initialized = true;
+		new (&_service) type{std::forward<Args>(args)...};
+	}
+	
+	bool _initialized = false;
+	typename std::aligned_storage<sizeof(type), alignof(type)>::type _service;
+};
+
+} // namespace detail
+
+template<typename T>
+struct ForkedLazy : detail::LazyBase<ForkedLazy<T>, T> {
+	explicit ForkedLazy(kgr::Container container) : _container{std::move(container)} {}
+	
+private:
+	friend struct detail::LazyBase<ForkedLazy<T>, T>;
+	kgr::Container _container;
+};
+
+template<typename T>
+struct Lazy : detail::LazyBase<Lazy<T>, T> {
+	explicit Lazy(kgr::Container& container) : _container{container} {}
+	
+private:
+	friend struct detail::LazyBase<Lazy<T>, T>;
+	kgr::Container& _container;
+};
+
 }

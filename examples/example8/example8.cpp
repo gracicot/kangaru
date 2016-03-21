aafa8efbbf9e7781f505d8a9436974f2bcec2ab8
Guillaume Racicot <gufideg@gmail.com>
2016-03-21 16:27:35 -0400

    added more helpful comments

--- examples/example8/example8.cpp
+++ examples/example8/example8.cpp
@@ -11,6 +11,7 @@
 using std::cout;
 using std::endl;
 
+// Here we are declaring two candy types
 struct Caramel {
 	Caramel() {
 		cout << "Caramel made" << endl;
@@ -31,15 +32,18 @@ struct GummyBear {
 	}
 };
 
+// Then, we are declaring two service definition for them
 struct CaramelService : kgr::Service<Caramel> {};
 struct GummyBearService : kgr::Service<GummyBear> {};
 
+// We will need the service map
 template<typename>
 struct ServiceMap;
 
 template<> struct ServiceMap<Caramel> : kgr::Map<CaramelService> {};
 template<> struct ServiceMap<GummyBear> : kgr::Map<GummyBearService> {};
 
+// CandyFactory, making candies and recepies
 struct CandyFactory {
 	CandyFactory(
 		kgr::Generator<CaramelService> myCaramelGenerator,
@@ -74,22 +78,35 @@ struct CandyFactoryService : kgr::SingleService<CandyFactory, kgr::Dependency<
 	kgr::InvokerService<ServiceMap>
 >> {};
 
+// a recepie
 void recepie(Caramel, GummyBear) {
 	cout << "A sweet recepie mixing Caramel and GummyBear completed" << endl;
 }
 
 int main() {
 	kgr::Container container;
+	
+	// We are making our factory
 	auto candyFactory = container.service<CandyFactoryService>();
 	
+	// this will print nothing, as there is no candy constructed
 	auto lazyGummyBear = candyFactory.makeGummyBear();
+	
+	// this will print "Caramel made"
 	auto caramel = candyFactory.makeCaramel();
 	
+	// This will print both "GummyBear made" and "yummy! GummyBear!"
 	lazyGummyBear->eat();
+	
+	// This will print "yummy! Caramel!"
 	caramel.eat();
 	
 	cout << endl << "== Let's make a recepie ==" << endl;
 	
+	// As there are new candy made, this will print:
+	// > Caramel made
+	// > GummyBear made
+	// > A sweet recepie mixing Caramel and GummyBear completed
 	candyFactory.mix(recepie);
 	
 	return 0;

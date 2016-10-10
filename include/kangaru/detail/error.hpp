1eea2a4f08d790405cf3d10b14fcebade6631cd2
Guillaume Racicot <gufideg@gmail.com>
2016-10-10 13:01:00 -0400

    removed useless semicolons

--- include/kangaru/detail/error.hpp
+++ include/kangaru/detail/error.hpp
@@ -18,7 +18,7 @@ struct ServiceError {
 			"One or more dependencies are not constructible with it's dependencies as constructor argument. "
 			"Please check that dependency's constructor and it's dependencies."
 		);
-	};
+	}
 	
 	template<typename U = T, enable_if_t<
 		is_service<U>::value &&
@@ -29,7 +29,7 @@ struct ServiceError {
 			"One or more dependencies are not constructible with it's dependencies as constructor argument. "
 			"Please check that dependency's constructor and dependencies."
 		);
-	};
+	}
 	
 	template<typename U, enable_if_t<
 		is_service<U>::value &&
@@ -40,7 +40,7 @@ struct ServiceError {
 			"The service type is not constructible given it's dependencies and passed arguments. "
 			"Ensure that dependencies are correctly configured and you pass the right set of parameters."
 		);
-	};
+	}
 	
 	template<typename U, enable_if_t<
 		is_service<U>::value &&
@@ -48,7 +48,7 @@ struct ServiceError {
 		is_service_constructible<T>::value, int> = 0>
 	ServiceError(U&&, ...) {
 		static_assert(false_t<U>::value, "The service type is not constructible given passed arguments to kgr::Container::service(...).");
-	};
+	}
 	
 	template<typename U = T, enable_if_t<is_service<U>::value && !is_service_constructible<U>::value, int> = 0>
 	ServiceError(...) {
@@ -56,17 +56,17 @@ struct ServiceError {
 			"The service type is not constructible given it's dependencies."
 			"Check if dependencies are configured correctly and if the service has the required constructor."
 		);
-	};
+	}
 	
 	template<typename U = T, enable_if_t<!is_service<U>::value && !has_forward<U>::value, int> = 0>
 	ServiceError(...) {
 		static_assert(false_t<U>::value, "The type sent to kgr::Container::service(...) is not a service.");
-	};
+	}
 	
 	template<typename U = T, enable_if_t<!is_service<U>::value && has_forward<U>::value, int> = 0>
 	ServiceError(...) {
 		static_assert(false_t<U>::value, "The service type must not not contain any virtual function or must be abstract.");
-	};
+	}
 };
 
 struct NotInvokableError {
@@ -76,7 +76,7 @@ struct NotInvokableError {
 			"The function sent is not invokable. Ensure to include all services definitions "
 			"you need and that received parameters are correct."
 		);
-	};
+	}
 };
 
 } // namespace detail

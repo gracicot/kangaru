64ba788ec61eae77b5b79db1f473fcf47f01f8fc
Guillaume Racicot <gufideg@gmail.com>
2016-11-07 21:03:40 -0500

    type traits that apply to depencies recursively are now using a common implementation

--- include/kangaru/detail/service_traits.hpp
+++ include/kangaru/detail/service_traits.hpp
@@ -31,15 +31,14 @@ using original_t = typename original<T>::type;
 template<std::size_t n, typename F>
 using injected_argument_t = original_t<function_argument_t<n, F>>;
 
-template<typename T, typename... Args>
-struct is_dependencies_services_helper {
-private:
+template<template<typename...> class Trait, typename T, typename... Args>
+struct dependency_trait_helper {
 	static std::true_type sink(...);
 	
 	template<typename U, typename... As, std::size_t... S>
 	static decltype(sink(
-		std::declval<enable_if_t<is_dependencies_services_helper<injected_argument_t<S, construct_function_t<U, As...>>>::type::value, int>>()...,
-		std::declval<enable_if_t<is_service<injected_argument_t<S, construct_function_t<U, As...>>>::value, int>>()...
+		std::declval<enable_if_t<dependency_trait_helper<Trait, injected_argument_t<S, construct_function_t<U, As...>>>::type::value, int>>()...,
+		std::declval<enable_if_t<Trait<injected_argument_t<S, construct_function_t<U, As...>>>::value, int>>()...
 	)) test(seq<S...>);
 	
 	template<typename U, typename...>
@@ -58,8 +57,8 @@ public:
 	using type = decltype(test_helper<T, Args...>(0));
 };
 
-template<typename T, typename... Args>
-using is_dependencies_services = typename is_dependencies_services_helper<T, Args...>::type;
+template<template<typename...> class Trait, typename T, typename... Args>
+using dependency_trait = typename dependency_trait_helper<Trait, T, Args...>::type;
 
 template<typename T, typename... Args>
 struct is_service_constructible_helper {
@@ -89,36 +88,6 @@ public:
 
 template<typename T, typename... Args>
 using is_service_constructible = typename is_service_constructible_helper<T, Args...>::type;
-
-template<typename T, typename... Args>
-struct is_dependencies_constructible_helper {
-private:
-	static std::true_type sink(...);
-	
-	template<typename U, typename... As, std::size_t... S>
-	static decltype(sink(
-		std::declval<enable_if_t<is_dependencies_constructible_helper<injected_argument_t<S, construct_function_t<U, As...>>>::type::value, int>>()...,
-		std::declval<enable_if_t<is_service_constructible<injected_argument_t<S, construct_function_t<U, As...>>>::type::value, int>>()...
-	)) test(seq<S...>);
-	
-	template<typename U, typename...>
-	static enable_if_t<is_container_service<U>::value || std::is_abstract<U>::value, std::true_type> test_helper(int);
-	
-	template<typename...>
-	static std::false_type test(...);
-	
-	template<typename...>
-	static std::false_type test_helper(...);
-	
-	template<typename U, typename... As>
-	static decltype(test<U, As...>(tuple_seq_minus<function_arguments_t<construct_function_t<U, As...>>, sizeof...(As)>{})) test_helper(int);
-	
-public:
-	using type = decltype(test_helper<T, Args...>(0));
-};
-
-template<typename T, typename... Args>
-using is_dependencies_constructible = typename is_dependencies_constructible_helper<T, Args...>::type;
 
 template<typename T>
 struct is_override_service_helper {
@@ -151,37 +120,6 @@ public:
 
 template<typename T>
 using is_override_convertible = typename is_override_convertible_helper<T>::type;
-
-template<typename T, typename... Args>
-struct is_dependencies_override_convertible_helper {
-private:
-	static std::true_type sink(...);
-	
-	template<typename U, typename... As, std::size_t... S>
-	static decltype(sink(
-		std::declval<enable_if_t<is_dependencies_override_convertible_helper<injected_argument_t<S, construct_function_t<U, As...>>>::type::value, int>>()...,
-		std::declval<enable_if_t<is_override_convertible<injected_argument_t<S, construct_function_t<U, As...>>>::type::value, int>>()...
-	)) test(seq<S...>);
-	
-	template<typename U, typename...>
-	static enable_if_t<is_container_service<U>::value || std::is_abstract<U>::value, std::true_type> test_helper(int);
-	
-	template<typename...>
-	static std::false_type test(...);
-	
-	template<typename...>
-	static std::false_type test_helper(...);
-	
-	template<typename U, typename... As>
-	static decltype(test<U, As...>(tuple_seq_minus<function_arguments_t<construct_function_t<U, As...>>, sizeof...(As)>{})) test_helper(int);
-	
-public:
-	using type = decltype(test_helper<T, Args...>(0));
-};
-
-template<typename T, typename... Args>
-using is_dependencies_override_convertible = typename is_dependencies_override_convertible_helper<T, Args...>::type;
-
 
 template<typename T, typename... Args>
 using is_single_no_args = std::integral_constant<bool,
@@ -238,12 +176,12 @@ template<typename T, typename... Args>
 using is_service_valid = std::integral_constant<bool, 
 	is_service<T>::value &&
 	is_single_no_args<T, Args...>::value &&
-	is_dependencies_services<T, Args...>::value &&
+	dependency_trait<is_service, T, Args...>::value &&
 	is_service_constructible<T, Args...>::value &&
-	is_dependencies_constructible<T, Args...>::value &&
+	dependency_trait<is_service_constructible, T, Args...>::value &&
 	is_default_service_valid<T>::value &&
-	is_override_convertible<T>::value &&
-	is_dependencies_override_convertible<T, Args...>::value
+	dependency_trait<is_override_convertible, T, Args...>::value &&
+	is_override_convertible<T>::value 
 >;
 
 } // namespace detail

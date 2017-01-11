f9b7e1b77aaf95fdb69bdf50790a8f99552e8f0e
Guillaume Racicot <gufideg@gmail.com>
2017-01-11 01:23:11 -0500

    added autocall traits

--- include/kangaru/detail/invoke.hpp
+++ include/kangaru/detail/invoke.hpp
@@ -34,6 +34,72 @@ struct AutoCallNoMap {
 
 namespace detail {
 
+template<typename T, typename F>
+struct autocall_function {
+private:
+	template<typename U, typename C>
+	struct get_member_autocall {
+		using type = std::integral_constant<
+			decltype(&U::template autocall<C, U::template Map>),
+			&U::template autocall<C, U::template Map>
+		>;
+	};
+	
+	template<typename U, typename C, std::size_t... S>
+	struct get_invoke_autocall {
+		using type = std::integral_constant<
+			decltype(&U::template autocall<C, detail::meta_list_element_t<S, typename C::Parameters>...>),
+			&U::template autocall<C, detail::meta_list_element_t<S, typename C::Parameters>...>
+		>;
+	};
+	
+	template<typename U, typename C, std::size_t... S>
+	static get_invoke_autocall<U, C, S...> test_helper(seq<S...>);
+	
+	template<typename U, typename C, enable_if_t<is_invoke_call<C>::value, int> = 0>
+	static decltype(test_helper<U, C>(tuple_seq<typename C::Parameters>{})) test();
+	
+	template<typename U, typename C, enable_if_t<is_member_autocall<U, C>::value, int> = 0>
+	static get_member_autocall<U, C> test();
+	
+	using inner_type = decltype(test<T, F>());
+	
+public:
+	using type = typename inner_type::type;
+};
+
+template<typename, typename, typename = void>
+struct autocall_arguments;
+
+template<typename T, typename F>
+struct autocall_arguments<T, F, enable_if_t<is_invoke_call<F>::value>> {
+	using type = typename F::Parameters;
+};
+
+template<typename T, typename F>
+struct autocall_arguments<T, F, enable_if_t<is_member_autocall<T, F>::value>> {
+private:
+	template<template<typename> class Map>
+	struct mapped_type {
+		template<typename U>
+		using map = service_map_t<Map, U>;
+	};
+	
+public:
+	using type = meta_list_transform_t<function_arguments_t<typename F::value_type>, mapped_type<T::template Map>::template map>;
+};
+
+template<typename T, typename F>
+using autocall_function_t = typename autocall_function<T, F>::type;
+
+template<typename T, typename F>
+using autocall_arguments_t = typename autocall_arguments<T, F>::type;
+
+template<typename T, typename F>
+using is_valid_autocall_function = std::integral_constant<bool,
+	is_invoke_call<F>::value || is_member_autocall<T, F>::value
+>;
+
 template<typename, typename = void>
 struct has_map_entry : std::false_type {};
 

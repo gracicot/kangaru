a40202d69b3230bbca0f7f5a4c6e3a45e58bd884
Guillaume Racicot <gufideg@gmail.com>
2018-06-16 14:41:26 -0400

    Reimplemented override_traits using the detection idiom

---
+++ include/kangaru/detail/detection.hpp
@@ -0,0 +1,76 @@
+#ifndef KGR_KANGARU_INCLUDE_KANGARU_DETAIL_DETECTION_HPP
+#define KGR_KANGARU_INCLUDE_KANGARU_DETAIL_DETECTION_HPP
+
+#include "meta_list.hpp"
+#include "void_t.hpp"
+#include "utils.hpp"
+
+namespace kgr {
+namespace detail {
+
+/*
+ * The following namespace contains the detection idiom implementation
+ */
+namespace detail_detection {
+
+template <typename Default, typename AlwaysVoid, template<typename...> typename Op, typename... Args>
+struct detector {
+	using value_t = std::false_type;
+	using type = Default;
+};
+
+template <typename Default, template<typename...> typename Op, typename... Args>
+struct detector<Default, void_t<Op<Args...>>, Op, Args...> {
+	using value_t = std::true_type;
+	using type = Op<Args...>;
+};
+
+template<typename Default, bool b, typename AlwaysVoid, template<typename...> class C, typename... Args>
+struct enabled_detector {
+	using type = Default;
+};
+
+template<typename Default, template<typename...> class C, typename... Args>
+struct enabled_detector<Default, false, void, C, Args...> {
+	using type = Default;
+};
+
+} // namespace detail
+
+struct nonesuch {
+	nonesuch() = delete;
+	~nonesuch() = delete;
+	nonesuch(nonesuch const&) = delete;
+	void operator=(nonesuch const&) = delete;
+};
+
+template <template<class...> class Op, class... Args>
+using is_detected = typename detail_detection::detector<nonesuch, void, Op, Args...>::value_t;
+
+template <template<class...> class Op, class... Args>
+using detected_t = typename detail_detection::detector<nonesuch, void, Op, Args...>::type;
+
+template <class Default, template<class...> class Op, class... Args>
+using detected_or = detail_detection::detector<Default, void, Op, Args...>;
+
+template<typename B>
+struct negation : bool_constant<!bool(B::value)> {};
+
+template<typename...> struct conjunction : std::true_type {};
+template<typename B1> struct conjunction<B1> : B1 {};
+template<typename B1, typename... Bn>
+struct conjunction<B1, Bn...> : std::conditional<bool(B1::value), conjunction<Bn...>, B1>::type {};
+
+template<typename T, template<typename...> class, typename...>
+struct all_of_traits : std::false_type {};
+
+template<typename... Types, template<typename...> class Trait, typename... Args>
+struct all_of_traits<meta_list<Types...>, Trait, Args...> : conjunction<Trait<Types, Args...>...> {};
+
+template<typename... Types, template<typename...> class Trait, typename... Args>
+struct all_of_traits<std::tuple<Types...>, Trait, Args...> : conjunction<Trait<Types, Args...>...> {};
+
+} // namespace detail
+} // namespace kgr
+
+#endif // KGR_KANGARU_INCLUDE_KANGARU_DETAIL_DETECTION_HPP
--- include/kangaru/detail/override_traits.hpp
+++ include/kangaru/detail/override_traits.hpp
@@ -3,188 +3,61 @@
 
 #include "traits.hpp"
 #include "meta_list.hpp"
+#include "detection.hpp"
 
 namespace kgr {
 namespace detail {
 
 /*
- * Trait that check if the service type returned by
- * overrides service definitions can be converted to the service type of the service T definition.
- */
-template<typename T>
-struct is_override_convertible_helper {
-private:
-	// This is a workaround for msvc. Expansion in very complex expression
-	// leaves the compiler without clues about what's going on.
-	template<std::size_t I, typename U>
-	struct expander {
-		using type = is_explicitly_convertible<service_type<U>, service_type<meta_list_element_t<I, parent_types<U>>>>;
-	};
-
-	template<typename...>
-	static std::false_type test(...);
-
-	template<typename...>
-	static std::false_type test_helper(...);
-	
-	template<typename U, std::size_t... S, int_t<enable_if_t<expander<S, U>::type::value>...> = 0>
-	static std::true_type test_helper(seq<S...>);
-	
-	template<typename U, std::size_t... S, int_t<enable_if_t<is_service<meta_list_element_t<S, parent_types<U>>>::value>...> = 0>
-	static decltype(test_helper<U>(seq<S...>{})) test(seq<S...>);
-	
-public:
-	using type = decltype(test<T>(tuple_seq<parent_types<T>>{}));
-};
-
-/*
- * Alias for is_override_convertible_helper
- */
-template<typename T>
-using is_override_convertible = typename is_override_convertible_helper<T>::type;
-
-/*
  * Trait that check if all types specified in overrides are services
  */
 template<typename T>
-struct is_override_services_helper {
-private:
-	// This is a workaround for msvc. Expansion in very complex expression
-	// leaves the compiler without clues about what's going on.
-	template<std::size_t I, typename U>
-	struct expander {
-		using type = is_service<meta_list_element_t<I, parent_types<U>>>;
-	};
-	
-	template<typename...>
-	static std::false_type test(...);
-
-	template<typename...>
-	static std::false_type test_helper(...);
-	
-	template<typename U, std::size_t... S, int_t<enable_if_t<expander<S, U>::type::value>...> = 0>
-	static std::true_type test_helper(seq<S...>);
-	
-	template<typename U, std::size_t... S, int_t<enable_if_t<is_service<meta_list_element_t<S, parent_types<U>>>::value>...> = 0>
-	static decltype(test_helper<U>(seq<S...>{})) test(seq<S...>);
-	
-public:
-	using type = decltype(test<T>(tuple_seq<parent_types<T>>{}));
-};
+using is_override_services = all_of_traits<parent_types<T>, is_service>;
 
 /*
- * Alias for is_override_services_helper
+ * Trait that check if the service type returned by
+ * one override's service definitions can be converted to the service type of the service T definition.
+ */
+template<typename Parent, typename T>
+using is_one_override_convertible = is_explicitly_convertible<service_type<T>, detected_t<service_type, Parent>>;
+
+/*
+ * Trait that check if the service type returned by
+ * all overriden service definitions can be converted to the service type of the service T definition.
  */
 template<typename T>
-using is_override_services = typename is_override_services_helper<T>::type;
+using is_override_convertible = all_of_traits<parent_types<T>, is_one_override_convertible, T>;
 
 /*
  * Trait that check if all overriden services are polymorphic
  */
 template<typename T>
-struct is_override_polymorphic_helper {
-private:
-	// This is a workaround for msvc. Expansion in very complex expression
-	// leaves the compiler without clues about what's going on.
-	template<std::size_t I, typename U>
-	struct expander {
-		using type = is_polymorphic<meta_list_element_t<I, parent_types<U>>>;
-	};
-	
-	template<typename...>
-	static std::false_type test(...);
-	
-	template<typename U, std::size_t... S, int_t<enable_if_t<expander<S, U>::type::value>...> = 0>
-	static std::true_type test(seq<S...>);
-	
-public:
-	using type = decltype(test<T>(tuple_seq<parent_types<T>>{}));
-};
-
-/*
- * Alias for is_override_virtual_helper
- */
-template<typename T>
-using is_override_polymorphic = typename is_override_polymorphic_helper<T>::type;
+using is_override_polymorphic = all_of_traits<parent_types<T>, is_polymorphic>;
 
 /*
  * Trait that check if no overriden services are final
  */
 template<typename T>
-struct is_override_not_final_helper {
-private:
-	// This is a workaround for msvc. Expansion in very complex expression
-	// leaves the compiler without clues about what's going on.
-	template<std::size_t I, typename U>
-	struct expander {
-		using type = is_final_service<meta_list_element_t<I, parent_types<U>>>;
-	};
-	
-	template<typename...>
-	static std::false_type test(...);
-	
-	template<typename U, std::size_t... S, int_t<enable_if_t<!expander<S, U>::type::value>...> = 0>
-	static std::true_type test(seq<S...>);
-	
-public:
-	using type = decltype(test<T>(tuple_seq<parent_types<T>>{}));
-};
-
-/*
- * Alias for is_override_virtual_helper
- */
-template<typename T>
-using is_override_not_final = typename is_override_not_final_helper<T>::type;
+using is_override_not_final = all_of_traits<parent_types<T>, is_not_final_service>;
 
 /*
  * Trait that check if the default service of an abstract service overrides that abstract service
  */
 template<typename T>
-struct is_default_overrides_abstract_helper {
-private:
-	template<typename>
-	static std::false_type test(...);
-	
-	template<typename U, enable_if_t<has_default<U>::value && is_abstract_service<U>::value, int> = 0>
-	static is_overriden_by<U, default_type<U>> test(int);
-	
-	template<typename U, enable_if_t<!has_default<U>::value, int> = 0>
-	static std::true_type test(int);
-	
-public:
-	using type = decltype(test<T>(0));
-};
-
-/*
- * Alias for is_default_overrides_abstract_helper
- */
-template<typename T>
-using is_default_overrides_abstract = typename is_default_overrides_abstract_helper<T>::type;
+using is_default_overrides_abstract = bool_constant<
+	!has_default<T>::value || is_overriden_by<T, detected_t<default_type, T>>::value
+>;
 
 /*
  * Trait that check if the default service type is convertible to the abstract service type.
  */
 template<typename T>
-struct is_default_convertible_helper {
-private:
-	template<typename>
-	static std::false_type test(...);
-	
-	template<typename U, enable_if_t<has_default<U>::value, int> = 0>
-	static is_explicitly_convertible<service_type<default_type<U>>, service_type<U>> test(int);
-	
-	template<typename U, enable_if_t<!has_default<U>::value, int> = 0>
-	static std::true_type test(int);
-	
-public:
-	using type = decltype(test<T>(0));
-};
-
-/*
- * Alias for is_default_convertible_helper
- */
-template<typename T>
-using is_default_convertible = typename is_default_convertible_helper<T>::type;
+using is_default_convertible = bool_constant<
+	!has_default<T>::value || is_explicitly_convertible<
+		detected_t<service_type, detected_t<default_type, T>>,
+		detected_t<service_type, T>
+	>::value
+>;
 
 } // namespace detail
 } // namespace kgr

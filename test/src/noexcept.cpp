13db507a0749e53a0506f0ac5ac022ba29e91374
Guillaume Racicot <gufideg@gmail.com>
2018-11-14 19:39:08 -0500

    Fix windows builds by setting the abort behavior

--- test/src/noexcept.cpp
+++ test/src/noexcept.cpp
@@ -2,17 +2,27 @@
 #include <kangaru/kangaru.hpp>
 #include <csignal>
 
-static void abort_handler(int signal)
-{
+
+
+static void abort_handler(int signal) {
 	if (signal == SIGABRT) {
 		SUCCEED();
 		std::_Exit(EXIT_SUCCESS);
 	}
 }
 
-TEST_CASE("Should abort when exceptions are disabled", "[noexcept]") {
+static void setup_handlers() {
 	std::signal(SIGABRT, abort_handler);
-	
+
+	// On windows we must prevent the "Abort, Retry, Ignore" window from opening.
+#if defined(_WIN32) || defined(_WIN64)
+	_set_abort_behavior(0, _WRITE_ABORT_MSG);
+#endif
+}
+
+TEST_CASE("Should abort when exceptions are disabled", "[noexcept]") {
+	setup_handlers();
+
 #if defined(KGR_KANGARU_TEST_ABSTRACT_ABORT)
 	SECTION("Supplied service error") {
 		struct S {};

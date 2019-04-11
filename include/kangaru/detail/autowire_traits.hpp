5b32784dec8ef8a76dd31f63a3c507712277affe
Guillaume Racicot <gufideg@gmail.com>
2019-04-11 00:02:24 -0400

    Removed C++14 extension

--- include/kangaru/detail/autowire_traits.hpp
+++ include/kangaru/detail/autowire_traits.hpp
@@ -196,7 +196,7 @@ constexpr default_inject_function{};
  * Will send as many deducers as there are numbers in S
  */
 template<typename Self, typename Map, typename I, std::size_t... S, typename... Args>
-inline auto deduce_construct(detail::seq<S...>, I inject, inject_t<container_service> cont, Args&&... args) -> decltype(auto) {
+inline auto deduce_construct(detail::seq<S...>, I inject, inject_t<container_service> cont, Args&&... args) -> detail::call_result_t<I, detail::deducer_expand_t<Self, Map, S>..., Args...> {
 	auto& container = cont.forward();
 
 	// The expansion of the inject call may be empty. This will silence the warning.

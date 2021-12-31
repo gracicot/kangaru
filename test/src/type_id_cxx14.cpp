ddc422c19c6e56b3ba7588c0d839a347d8be6242
Guillaume Racicot <gufideg@gmail.com>
2021-12-31 11:46:06 -0300

    Properly placed test in namespace scope

--- test/src/type_id_cxx14.cpp
+++ test/src/type_id_cxx14.cpp
@@ -6,15 +6,15 @@
 
 namespace kgr_test {
 
+struct test {};
+class another_type;
+
 TEST_CASE("The hash based typeid give back reproducible values", "[type_id]") {
-	struct test;
-	class another_type;
+	REQUIRE(kgr::detail::type_name<test>() == "kgr_test::test");
+	REQUIRE(kgr::type_id<test>() == ((kgr::detail::hash_64_fnv1a("kgr_test::test") & kgr::detail::hash_mask)));
 
-	REQUIRE(kgr::detail::type_name<test>() == "test");
-	REQUIRE(kgr::type_id<test>() == ((kgr::detail::hash_64_fnv1a("test") & kgr::detail::hash_mask)));
-
-	REQUIRE(kgr::detail::type_name<another_type>() == "another_type");
-	REQUIRE(kgr::type_id<another_type>() == ((kgr::detail::hash_64_fnv1a("another_type") & kgr::detail::hash_mask)));
+	REQUIRE(kgr::detail::type_name<another_type>() == "kgr_test::another_type");
+	REQUIRE(kgr::type_id<another_type>() == ((kgr::detail::hash_64_fnv1a("kgr_test::another_type") & kgr::detail::hash_mask)));
 
 	REQUIRE(kgr::detail::type_name<int>() == "int");
 	REQUIRE(kgr::type_id<int>() == ((kgr::detail::hash_64_fnv1a("int") & kgr::detail::hash_mask)));

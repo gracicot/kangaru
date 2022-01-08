a5ae52b34b5e362fff410162b4ea46f6db94f22e
Guillaume Racicot <gufideg@gmail.com>
2022-01-07 21:04:09 -0300

    Added tests for static_assert message

---
+++ test/src/error.cpp
@@ -0,0 +1,20 @@
+#include <kangaru/kangaru.hpp>
+
+#define TEST_ERROR(...) int toplevel = (void(__VA_ARGS__), 0)
+
+#if defined(kgr_error_test_not_service_no_forward)
+	
+	struct test {};
+	TEST_ERROR(kgr::debug::service<test>());
+	
+#elif defined(kgr_error_test_not_service_has_forward)
+	
+	struct test {
+		virtual ~test() = default;
+		auto forward() -> int;
+	};
+	TEST_ERROR(kgr::debug::service<test>());
+	
+#endif
+
+static_assert(false, "This file never compiles");

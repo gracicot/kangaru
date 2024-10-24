19fe7238499875c3138b4c82b73dc27e9c4c620d
Guillaume Racicot <dev@gracicot.com>
2024-10-24 13:05:15 -0400

    Clang 13 compatible

--- include/kangaru/detail/container.hpp
+++ include/kangaru/detail/container.hpp
@@ -33,39 +33,54 @@ namespace kangaru {
 		
 		container_source_t source;
 		
-		static constexpr auto rebound_source_tree(auto&& source) {
+		constexpr auto rebound_source_tree() & {
 			return with_recursion{
 				kangaru::make_source_with_exhaustive_construction(
 					with_alternative{
 						with_recursion{
 							with_source_from_tag{
-								KANGARU5_NO_ADL(ref)(source.source)
+								KANGARU5_NO_ADL(fwd_ref)(source)
 							}
 						},
-						external_reference_source<dynamic_container>{source}
+						external_reference_source<dynamic_container>{*this}
+					}
+				)
+			};
+		}
+		
+		constexpr auto rebound_source_tree() && {
+			return with_recursion{
+				kangaru::make_source_with_exhaustive_construction(
+					with_alternative{
+						with_recursion{
+							with_source_from_tag{
+								KANGARU5_NO_ADL(fwd_ref)(std::move(source))
+							}
+						},
+						external_reference_source<dynamic_container>{*this}
 					}
 				)
 			};
 		}
 		
 		template<typename Self>
-		using rebound_source_tree_t = decltype(rebound_source_tree(std::declval<Self>()));
+		using rebound_source_tree_t = decltype(std::declval<Self>().rebound_source_tree());
 		
 		template<typename T>
 		static constexpr auto do_provide(auto&& source) -> T {
 			return kangaru::provide(
-				provide_tag_v<T>, rebound_source_tree(KANGARU5_FWD(source))
+				provide_tag_v<T>, KANGARU5_FWD(source).rebound_source_tree()
 			);
 		}
 		
 	public:
-		template<typename T> requires source_of<rebound_source_tree_t<dynamic_container&>, T>
-		constexpr auto provide() & -> T {
+		template<typename T> 
+		constexpr auto provide() & -> T requires source_of<rebound_source_tree_t<dynamic_container&>, T> {
 			return do_provide<T>(*this);
 		}
 		
-		template<typename T> requires source_of<rebound_source_tree_t<dynamic_container&&>, T>
-		constexpr auto provide() && -> T {
+		template<typename T>
+		constexpr auto provide() && -> T requires source_of<rebound_source_tree_t<dynamic_container&&>, T> {
 			return do_provide<T>(std::move(*this));
 		}
 	};
